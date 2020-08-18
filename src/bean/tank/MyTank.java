65d601461f307e71ca855479bfaea91e3a9bf373
renyanzhen <ren1151576@qq.com>
2020-08-18 11:12:09 +0800

    还是基础移动

--- src/bean/tank/MyTank.java
+++ src/bean/tank/MyTank.java
@@ -7,21 +7,40 @@ import javafx.scene.input.KeyCode;
 
 public final class MyTank extends Tank {
     public MyTank() {
-        super(new Image("FIle:image/自己.png"), 1, 1);
-
+        super(new Image("FIle:image/自己.png"), 1, 1, Direction.up);
         Game.scene.setOnKeyPressed(event -> {
             KeyCode code = event.getCode();
             switch (code) {
                 case W:
+                    if (direction != Direction.up) {
+                        imageView.setRotate(0);
+                        direction = Direction.up;
+                        break;
+                    }
                     moveUp();
                     break;
                 case S:
+                    if (direction != Direction.down) {
+                        imageView.setRotate(180);
+                        direction = Direction.down;
+                        break;
+                    }
                     moveDown();
                     break;
                 case A:
+                    if (direction != Direction.left) {
+                        imageView.setRotate(270);
+                        direction = Direction.left;
+                        break;
+                    }
                     moveLeft();
                     break;
                 case D:
+                    if (direction != Direction.right) {
+                        imageView.setRotate(90);
+                        direction = Direction.right;
+                        break;
+                    }
                     moveRight();
                     break;
                 default:
--- src/bean/tank/Tank.java
+++ src/bean/tank/Tank.java
@@ -8,8 +8,17 @@ public abstract class Tank extends MoveAbleObject {
 
     private Bullet bullet;
 
-    public Tank(Image image, int x, int y) {
+    protected Direction direction;
+
+    public Tank(Image image, int x, int y, Direction direction) {
+        this.direction = direction;
         initElement(image, x, y);
     }
 
+    protected enum Direction{
+        up,
+        down,
+        left,
+        right
+    }
 }
