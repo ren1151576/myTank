bd16d41288643ffb3fd2f7c7672946b586d8dfc2
renyanzhen <ren1151576@qq.com>
2020-08-21 17:14:44 +0800

    键盘防抖

--- src/bean/tank/MyTank.java
+++ src/bean/tank/MyTank.java
@@ -4,8 +4,15 @@ import bean.game.Direction;
 import bean.game.Game;
 import bean.game.Type;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public final class MyTank extends Tank {
+
+    private final MyTankMove myTankMove = new MyTankMove();
+
     public MyTank() {
         super(new Image("FIle:image/自己.png"), 1, 1, Direction.up);
         Game.scene.setOnKeyPressed(event -> {
@@ -15,7 +22,7 @@ public final class MyTank extends Tank {
                     fire();
                     break;
                 default:
-                    move(event.getCode());
+                    myTankMove.myMove(event.getCode());
             }
 
         });
@@ -24,5 +31,23 @@ public final class MyTank extends Tank {
     @Override
     protected Type setType() {
         return Type.myTank;
+    }
+
+    private class MyTankMove extends TimerTask {
+        private Timer timer;
+
+        public void myMove(KeyCode keyCode) {
+            if (timer != null) {
+                return;
+            }
+            move(keyCode);
+            timer = new Timer();
+            timer.schedule(myTankMove, 1000);
+        }
+
+        @Override
+        public void run() {
+            timer = null;
+        }
     }
 }
